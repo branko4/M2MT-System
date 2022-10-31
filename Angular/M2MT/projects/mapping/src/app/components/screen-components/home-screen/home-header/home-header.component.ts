4382a21079aa2214f224b009e6bf384776e0a481
branko <50580893+branko4@users.noreply.github.com>
2022-10-31 10:20:59 +0100

    Cleaned up styling, add dark and light theme

--- Angular/M2MT/projects/mapping/src/app/components/screen-components/home-screen/home-header/home-header.component.ts
+++ Angular/M2MT/projects/mapping/src/app/components/screen-components/home-screen/home-header/home-header.component.ts
@@ -1,15 +1,48 @@
 import { Component, OnInit } from '@angular/core';
 
+enum Themes {
+  DEFAULT="default",
+  LIGHT="light-mode",
+  DARK="dark-mode",
+}
+
 @Component({
   selector: 'app-home-header',
   templateUrl: './home-header.component.html',
   styleUrls: ['./home-header.component.scss']
 })
 export class HomeHeaderComponent implements OnInit {
+  public static readonly THEME_STORAGE_NAME = "THEME_STORAGE";
+  readonly themeOptions: Themes[] = [
+    Themes.DARK,
+    Themes.DEFAULT,
+    Themes.LIGHT,
+  ]
 
-  constructor() { }
+  theme: Themes = Themes.DEFAULT;
+
+  menuOpen = false;
 
   ngOnInit(): void {
+    const inStorage = localStorage.getItem(HomeHeaderComponent.THEME_STORAGE_NAME);
+    if (!inStorage) {
+      this.theme = Themes.DEFAULT;
+      return;
+    }
+
+    this.theme = <Themes> inStorage;
+    this.updateThem(<Themes> inStorage)
   }
 
+  updateThem(newTheme: Themes) {
+    console.log(newTheme);
+    if (this.theme !== Themes.DEFAULT) document.body.classList.remove(this.theme);
+    this.theme = newTheme;
+    document.body.classList.add(this.theme);
+    localStorage.setItem(HomeHeaderComponent.THEME_STORAGE_NAME, this.theme);
+  }
+
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen;
+  }
 }
